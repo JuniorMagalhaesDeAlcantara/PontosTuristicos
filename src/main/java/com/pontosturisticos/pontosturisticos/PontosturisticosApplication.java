bbf9531eb0b14b629669ed88d8afb02d9a74e8fa
Junior Magalhães <jrlevita09@hotmail.com>
2022-07-25 18:50:33 -0300

    Estrutura do Projeto

---
+++ src/main/java/com/pontosturisticos/pontosturisticos/PontosturisticosApplication.java
@@ -0,0 +1,13 @@
+package com.pontosturisticos.pontosturisticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontosturisticosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontosturisticosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pontosturisticos/pontosturisticos/PontosturisticosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontosturisticos.pontosturisticos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontosturisticosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
