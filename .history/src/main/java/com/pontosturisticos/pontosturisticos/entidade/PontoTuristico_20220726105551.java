259ca43ebafd2e4dcf04254dc7c9621177ed94e7
Junior Magalhães <jrlevita09@hotmail.com>
2022-07-26 11:06:05 -0300

    Criação de Entidade

---
+++ .history/src/main/java/com/pontosturisticos/pontosturisticos/entidade/PontoTuristico_20220726105551.java
@@ -0,0 +1,53 @@
+package com.pontosturisticos.pontosturisticos.entidade;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class PontoTuristico {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String cidade;
+    @Column(nullable = false)
+    private String localizacao;
+    @Column(nullable = false)
+    private String descricao;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getLocalizacao() {
+        return localizacao;
+    }
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
